3879c0aa248b283e1b0d5569586b121e86ca4389
mayuriprasad <maprasad@calpoly.edu>
2023-06-10 11:47:45 -0700

    tentative edits to integrate posenet and wheel spin NOT TESTED

--- pyglet_demo/cameraInput.py
+++ pyglet_demo/cameraInput.py
@@ -1,13 +1,54 @@
 import time
 
 class Input:
-    def __init__(self, on_trigger):
+    def __init__(self, on_trigger, net, input, output, args):
         self.on_trigger = on_trigger
+        self.net = net
+        self.input = input
+        self.output = output
+        self.args = args
 
     def update(self):
         threshold = 0
 
-        # Do some kind of camera logic here
+        while True:
+            # capture the next image
+            img = self.input.Capture()
+
+            if img is None: # timeout
+                continue  
+
+            # perform pose estimation (with overlay)
+            poses = self.net.Process(img, overlay=self.args.overlay)
+
+            # print the pose results
+            print("detected {:d} objects in image".format(len(poses)))
+
+            for pose in poses:
+                right_elbow_idx = pose.FindKeypoint('right_elbow')
+                left_elbow_idx = pose.FindKeypoint('left_elbow')
+
+                # if the keypoint index is < 0, it means it wasn't found in the image
+                if right_elbow_idx < 0 or left_elbow_idx < 0:
+                    continue
+                
+                left_elbow = pose.Keypoints[left_elbow_idx]
+                right_elbow = pose.Keypoints[right_elbow_idx]
+
+                #check if y value is above a certain point? if yes set threshold over 1
+
+            # render the image
+            self.output.Render(img)
+
+            # update the title bar
+            self.output.SetStatus("{:s} | Network {:.0f} FPS".format(self.args.network, self.net.GetNetworkFPS()))
+
+            # print out performance info
+            self.net.PrintProfilerTimes()
+
+            # exit on input/output EOS
+            if not input.IsStreaming() or not self.output.IsStreaming():
+                break
 
         if threshold > 1:
             self.on_trigger()
--- pyglet_demo/main.py
+++ pyglet_demo/main.py
@@ -7,7 +7,37 @@ import configs
 import app
 import cameraInput
 
+import sys
+import argparse
+
+from jetson_inference import poseNet
+from jetson_utils import videoSource, videoOutput, Log
+
 def main():
+    parser = argparse.ArgumentParser(description="Run pose estimation DNN on a video/image stream.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, 
+                                 epilog=poseNet.Usage() + videoSource.Usage() + videoOutput.Usage() + Log.Usage())
+
+    parser.add_argument("input", type=str, default="", nargs='?', help="URI of the input stream")
+    parser.add_argument("output", type=str, default="", nargs='?', help="URI of the output stream")
+    parser.add_argument("--network", type=str, default="resnet18-body", help="pre-trained model to load (see below for options)")
+    parser.add_argument("--overlay", type=str, default="links,keypoints", help="pose overlay flags (e.g. --overlay=links,keypoints)\nvalid combinations are:  'links', 'keypoints', 'boxes', 'none'")
+    parser.add_argument("--threshold", type=float, default=0.15, help="minimum detection threshold to use") 
+
+    try:
+        args = parser.parse_known_args()[0]
+    except:
+        print("")
+        parser.print_help()
+        sys.exit(0)
+
+    # load the pose estimation model
+    net = poseNet(args.network, sys.argv, args.threshold)
+
+    # create video sources & outputs
+    input = videoSource(args.input, argv=sys.argv)
+    output = videoOutput(args.output, argv=sys.argv)
+
     gl_background_color = tuple(map(lambda x : x / 255.0, configs.BACKGROUND_COLOR))
 
     config = pyglet.gl.Config(sample_buffers=1, samples=8)
