8b74636bcc99c8611498059c102bc877776c58e1
Christian Honein <61818445+chonein@users.noreply.github.com>
2024-02-03 11:27:42 -0800

    added to debug startup

--- pyglet_demo/main.py
+++ pyglet_demo/main.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 APPS = {
     "Magic Caboose": ["env/bin/python3", "magic_caboose/magic_caboose.py"],
@@ -7,24 +8,27 @@ APPS = {
 
 
 def main():
+    log_file_path = os.path.expanduser("~/main.log")
+    log_file = open(log_file_path, "w")
     next_app = "App Selector"
     while next_app is not None:
         if next_app == "App Selector":
-            print("App Selector launched")
+            print("App Selector launched", file=log_file)
             app_selector_cmd = ["env/bin/python3", "app_selector/app_selector.py"]
             app_selector_cmd.extend(list(APPS.keys()))
             process = subprocess.run(app_selector_cmd, capture_output=True)
-            print("App Selector exited")
+            print(process)
+            print("App Selector exited", file=log_file)
             process_stdout = str(process.stdout, encoding="utf-8").strip().split("\n")
             if len(process_stdout) < 0 or process_stdout[-1] not in APPS:
                 break
             else:
                 next_app = process_stdout[-1]
         else:
-            print(f"{next_app} launched")
+            print(f"{next_app} launched", file=log_file)
             process = subprocess.run(APPS[next_app])
             next_app = "App Selector"
-            print(f"{next_app} exited")
+            print(f"{next_app} exited", file=log_file)
 
 
 if __name__ == "__main__":
